f892ca4a2009e97b9f5325d30607539807291499
James Hardy <jamesjhardy95@gmail.com>
2023-05-04 12:00:23 +0100

    update

--- waylo-site/src/index.js
+++ waylo-site/src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 import App from './App';
+import Marquee from 'react-fast-marquee';
 import { Logo } from './Logo';
 
 function Overlay() {
@@ -16,47 +17,104 @@ function Overlay() {
         overflow: 'hidden',
       }}
     >
+      <Marquee speed={50}>
+        <p
+          className="item"
+          style={{
+            animation: 'scroll-left 15s linear infinite',
+            fontSize: '16px',
+            whiteSpace: 'nowrap',
+            width: '100%',
+          }}
+        >
+          <span
+            style={{
+              fontSize: '18px',
+              fontFamily: 'Eurostile-Wide',
+            }}
+          >
+            WAY
+          </span>
+          <span
+            style={{
+              fontSize: '18px',
+              fontFamily: 'Times',
+              fontStyle: 'italic',
+            }}
+          >
+            001
+          </span>
+          {'  '}
+          {'  '}
+          <span style={{ fontSize: '16px', fontFamily: 'SFPro' }}>
+            Event - 7th July - The Carpet Shop Peckham - Parris - Mosca - Nadí -
+            Holloway
+          </span>
+        </p>
+      </Marquee>
       <p
         style={{
-          animation: 'scroll-left 15s linear infinite',
-          fontSize: '13px',
+          position: 'absolute',
+          bottom: 20,
+          left: 90,
+          fontSize: '16px',
         }}
       >
-        WAY001 - Holloway - Out 01.01.01
+        <span
+          style={{
+            fontSize: '16px',
+            fontFamily: 'Eurostile-Wide',
+          }}
+        >
+          WAY
+        </span>
+        <span
+          style={{ fontSize: '16px', fontFamily: 'Times', fontStyle: 'italic' }}
+        >
+          001
+        </span>
+        <br />
+        <span style={{ fontSize: '16px', fontFamily: 'SFPro' }}>Holloway</span>
       </p>
       <a
-        href="https://pmnd.rs/"
         style={{
           position: 'absolute',
-          bottom: 40,
-          left: 90,
-          fontSize: '13px',
+          fontFamily: 'Eurostile-Wide',
+          top: 100,
+          left: 40,
+          fontSize: '16px',
         }}
-      >
-        WAY001
-        <br />
-        Holloway
-      </a>
-      <div
-        style={{ position: 'absolute', top: 50, left: 40, fontSize: '13px' }}
+        href="https://waylo-records.bandcamp.com/releases"
+        target="_blank"
       >
         Bandcamp —
-      </div>
-      <div
-        style={{ position: 'absolute', top: 75, left: 40, fontSize: '13px' }}
-      >
-        Spotify —
-      </div>
-      <div
-        style={{ position: 'absolute', top: 100, left: 40, fontSize: '13px' }}
+      </a>
+      <a
+        style={{
+          position: 'absolute',
+          fontFamily: 'Eurostile-Wide',
+          top: 125,
+          left: 40,
+          fontSize: '16px',
+        }}
+        href="https://www.instagram.com/waylo_records/"
+        target="_blank"
       >
         Instagram —
-      </div>
-      <div
-        style={{ position: 'absolute', top: 125, left: 40, fontSize: '13px' }}
+      </a>
+      <a
+        style={{
+          position: 'absolute',
+          fontFamily: 'Eurostile-Wide',
+          top: 150,
+          left: 40,
+          fontSize: '16px',
+        }}
+        href="https://ra.co/events/1692787"
+        target="_blank"
       >
         RA —
-      </div>
+      </a>
     </div>
   );
 }
